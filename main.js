a19016f75c2154f09bf00164ac589e438a939a15
aalaa-waleed <aalaa.waleed.a@gmail.com>
2024-12-15 16:59:12 +0200

    BloomUpload

---
+++ main.js
@@ -0,0 +1,12 @@
+function popup() {
+    window.alert("I hope you like our site🥺")
+}
+// function popup1() {
+//     window.alert("I hope you like our site🥺")
+// }
+// function popup2() {
+//     window.alert("Our team is ready to provide you with the best solutions🫡")
+// }
+// function popup3() {
+//     window.alert("Our products combine quality and innovation👌")
+// }
